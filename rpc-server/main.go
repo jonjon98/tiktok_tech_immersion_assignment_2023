dee5d15ae2f0e3550c659337c55c2cd985ca5c1c
jonjon98 <yapzh.jon@gmail.com>
2023-06-05 17:18:44 +0800

    feat: adds the basic project requirements.

--- rpc-server/main.go
+++ rpc-server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	rpc "github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc/imservice"
@@ -9,18 +11,30 @@ import (
 	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
+var (
+	rdb = &RedisClient{} // make the RedisClient with global visibility in the 'main' scope
+)
+
 func main() {
+	ctx := context.Background() // https://www.digitalocean.com/community/tutorials/how-to-use-contexts-in-go
+
+	err := rdb.InitClient(ctx, "redis:6379", "")
+	if err != nil {
+		 errMsg := fmt.Sprintf("failed to init Redis client, err: %v", err)
+		 log.Fatal(errMsg)
+	}
+
 	r, err := etcd.NewEtcdRegistry([]string{"etcd:2379"}) // r should not be reused.
 	if err != nil {
-		log.Fatal(err)
+		 log.Fatal(err)
 	}
 
 	svr := rpc.NewServer(new(IMServiceImpl), server.WithRegistry(r), server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
-		ServiceName: "demo.rpc.server",
+		 ServiceName: "demo.rpc.server",
 	}))
 
 	err = svr.Run()
 	if err != nil {
-		log.Println(err.Error())
+		 log.Println(err.Error())
 	}
 }
